aa93b49ff0e181e2265d7f61a67d70e6029bdad9
RicardoRei <ricardo.rei@unbabel.com>
2021-02-09 16:34:08 +0000

    bump version 0.0.7

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="unbabel-comet",
-    version="0.0.6.post1",
+    version="0.0.7",
     author="Ricardo Rei, Craig Stewart, Catarina Farinha, Alon Lavie",
     download_url="https://github.com/Unbabel/COMET",
     author_email="ricardo.rei@unbabel.com, craig.stewart@unbabel.com, catarina.farinha@unbabel.com, alon.lavie@unbabel.com",
     packages=find_packages(exclude=["tests"]),
     description="High-quality Machine Translation Evaluation",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     keywords=[
         "Deep Learning",
         "PyTorch",
