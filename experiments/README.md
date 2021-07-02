9f5fd7780f9c2d167bb16f72ca0709225635bec1
RicardoRei <ricardo.rei@unbabel.com>
2021-07-02 11:55:10 +0000

    refactor

--- experiments/README.md
+++
@@ -1,1 +0,0 @@
-Folder used to save checkpoints and tensorboard logs.
--- tests/unit/encoders/test_xlmr.py
+++ tests/unit/encoders/test_xlmr.py
@@ -1,68 +1,33 @@
 # -*- coding: utf-8 -*-
 import unittest
-from argparse import Namespace
 
-import torch
-
-from comet.models.encoders import XLMREncoder
+from comet.encoders.xlmr import XLMREncoder
 
 
 class TestXLMREncoder(unittest.TestCase):
-    """
-    There is not much we can test in our encoders...
 
-    The only thing we are interested is in maintaining a well defined interface
-    between all encoders.
-    """
-
-    hparams = Namespace(pretrained_model="xlmr.base")
-    model_base = XLMREncoder.from_pretrained(hparams)
-
-    def test_layerwise_lr(self):
-        expected_lr = [
-            0.1,
-            0.1,
-        ]  # LM Head and Last layer with 0.1 and all others with decay
-        expected_lr += [0.1 * (0.95 ** i) for i in range(1, 13)]  # LR for other layers
-        expected_lr += [
-            0.1 * (0.95 ** 13),
-            0.1 * (0.95 ** 13),
-            0.1 * (0.95 ** 13),
-        ]  # LR for embeddings (pos, word, layer_norm)
-        parameter_groups = self.model_base.layerwise_lr(0.1, 0.95)
-        param_lrs = [params["lr"] for params in parameter_groups[::-1]]
-        self.assertListEqual(expected_lr, param_lrs)
+    xlmr = XLMREncoder.from_pretrained("xlm-roberta-base")
 
     def test_num_layers(self):
-        self.assertEqual(self.model_base.num_layers, 13)
+        self.assertEqual(self.xlmr.num_layers, 13)
 
     def test_output_units(self):
-        self.assertEqual(self.model_base.output_units, 768)
+        self.assertEqual(self.xlmr.output_units, 768)
 
     def test_max_positions(self):
-        self.assertEqual(self.model_base.max_positions, 512)
+        self.assertEqual(self.xlmr.max_positions, 514)
 
     def test_prepare_sample(self):
         sample = ["hello world, welcome to COMET!", "This is a batch"]
-
-        model_input = self.model_base.prepare_sample(sample)
-        self.assertIn("tokens", model_input)
-        self.assertIn("lengths", model_input)
-
-        # Sanity Check: This is already checked when testing the tokenizer.
-        expected = self.model_base.model.encode(sample[0])
-        self.assertTrue(torch.equal(expected, model_input["tokens"][0]))
-        self.assertEqual(len(expected), model_input["lengths"][0])
+        model_input = self.xlmr.prepare_sample(sample)
+        self.assertIn("input_ids", model_input)
+        self.assertIn("attention_mask", model_input)
 
     def test_forward(self):
-        sample = ["hello world!", "This is a batch"]
-        model_input = self.model_base.prepare_sample(sample)
-        model_out = self.model_base(**model_input)
-        self.assertIn("wordemb", model_out)
-        self.assertIn("sentemb", model_out)
-        self.assertIn("all_layers", model_out)
-        self.assertIn("mask", model_out)
-        self.assertIn("extra", model_out)
-        self.assertEqual(len(model_out["all_layers"]), self.model_base.num_layers)
-        self.assertEqual(self.model_base.output_units, model_out["sentemb"].size()[1])
-        self.assertEqual(self.model_base.output_units, model_out["wordemb"].size()[2])
+        sample = ["hello world, welcome to COMET!", "This is a batch"]
+        model_input = self.xlmr.prepare_sample(sample)
+        model_output = self.xlmr(**model_input)
+        self.assertIn("wordemb", model_output)
+        self.assertIn("sentemb", model_output)
+        self.assertIn("all_layers", model_output)
+        self.assertIn("attention_mask", model_output)
