e85df9b4d0ce5d362778389399bed87be0f5931c
Created by Ansible <ricardo.rei@unbabel.com>
2021-08-10 21:17:37 +0000

    update on torchmetrics and test modelnames

--- tests/unit/test_download_utils.py
+++ tests/unit/test_download_utils.py
@@ -10,18 +10,18 @@ from comet.models import load_from_checkpoint
 class TestDownloadModel(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree(os.path.join(DATA_PATH, "wmt21-small-da-152012"))
+        shutil.rmtree(os.path.join(DATA_PATH, "wmt21-cometinho-da"))
 
     def test_download_from_s3(self):
-        data_path = download_model("wmt21-small-da-152012", saving_directory=DATA_PATH)
+        data_path = download_model("wmt21-cometinho-da", saving_directory=DATA_PATH)
         self.assertTrue(
             os.path.exists(
-                os.path.join(DATA_PATH, "wmt21-small-da-152012/hparams.yaml")
+                os.path.join(DATA_PATH, "wmt21-cometinho-da/hparams.yaml")
             )
         )
         self.assertTrue(
             os.path.exists(
-                os.path.join(DATA_PATH, "wmt21-small-da-152012/checkpoints/")
+                os.path.join(DATA_PATH, "wmt21-cometinho-da/checkpoints/")
             )
         )
         load_from_checkpoint(data_path)
