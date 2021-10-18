851602bb5fcd6cb3e8b3b0d0acdb567ef49eae1f
Ricardo Rei <ricardo.rei@unbabel.com>
2021-10-18 15:12:44 +0100

    bump version rc7

--- comet/__init__.py
+++ comet/__init__.py
@@ -23,5 +23,5 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
 
-__version__ = "1.0.0rc6"
+__version__ = "1.0.0rc7"
 __copyright__ = "2020-2021 Unbabel. All rights reserved."
