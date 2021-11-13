5cf5222b9cbba6622c5ef98b6e2f37dd71a76b47
Created by Ansible <ricardo.rei@unbabel.com>
2021-11-13 16:41:25 +0000

    poetry requirements and version update

--- comet/__init__.py
+++ comet/__init__.py
@@ -23,5 +23,5 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
 
-__version__ = "1.0.0rc9"
-__copyright__ = "2020-2021 Unbabel. All rights reserved."
+__version__ = "1.0.0"
+__copyright__ = "2020 Unbabel. All rights reserved."
