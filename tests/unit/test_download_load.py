659c685efb0ca3af988810f2e417b718d5f6bf91
BramVanroy <bram.vanroy@kuleuven.be>
2023-06-27 15:39:51 +0200

    re-add aws check

--- tests/unit/test_download_load.py
+++ tests/unit/test_download_load.py
@@ -19,3 +19,7 @@ class TestDownloadModel(unittest.TestCase):
 
     def test_download_fail(self):
         self.assertRaises(KeyError, download_model, "this_model_does_not_exist")
+
+    def test_download_from_hf(self):
+        data_path = download_model("Unbabel/wmt22-comet-da", saving_directory=DATA_PATH)
+        load_from_checkpoint(data_path)
