2d6d858a2fbc3f3ba9159a83d1302b5575a9ac0f
BramVanroy <bram.vanroy@kuleuven.be>
2023-06-27 14:36:03 +0200

    add legacy download test

--- tests/unit/test_download_load.py
+++ tests/unit/test_download_load.py
@@ -11,6 +11,11 @@ class TestDownloadModel(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(os.path.join(DATA_PATH, "models--Unbabel--wmt22-comet-da"))
+        shutil.rmtree(os.path.join(DATA_PATH, "eamt22-cometinho-da"))
+
+    def test_download_from_aws(self):
+        data_path = download_model("eamt22-cometinho-da", saving_directory=DATA_PATH)
+        load_from_checkpoint(data_path)
 
     def test_download_from_hf(self):
         data_path = download_model("Unbabel/wmt22-comet-da", saving_directory=DATA_PATH)
