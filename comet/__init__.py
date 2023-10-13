a31c2de3b93d07e364e57e61556bb0b7fe04374a
RicardoRei <ricardorei@unbabel.com>
2023-10-13 21:09:37 +0000

    Fix minor dependencies and bumped version

--- comet/__init__.py
+++ comet/__init__.py
@@ -22,5 +22,5 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __copyright__ = "2020 Unbabel. All rights reserved."
